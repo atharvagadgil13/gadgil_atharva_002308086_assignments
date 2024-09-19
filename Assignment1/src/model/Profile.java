923e3b5aef9bad08457b47bbc23c7b3c112fe5d3
atharvagadgil13 <gadgil.at@northeastern.edu>
2024-09-19 18:12:33 -0400

    Uploaded Assignment1

---
+++ Assignment1/src/model/Profile.java
@@ -0,0 +1,193 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Atharva
+ */
+public class Profile {
+    String FirstName;
+    String LastName;
+    String Email;
+    String Phone;
+    String DriversLicenseNumber;
+    String DriversLicenseState;
+    String SocialSecurityNumber;
+    String AddressLine1;
+    String AddressLine2;
+    String City;
+    String State;
+    String ZIP;
+    String Telephone;
+    String FaxNumber;
+    String Gender;
+    String DOB;
+    String OriginCountry;
+    String Ethnicity;
+    String PassportNumber;
+    String Language;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getDriversLicenseNumber() {
+        return DriversLicenseNumber;
+    }
+
+    public void setDriversLicenseNumber(String DriversLicenseNumber) {
+        this.DriversLicenseNumber = DriversLicenseNumber;
+    }
+
+    public String getDriversLicenseState() {
+        return DriversLicenseState;
+    }
+
+    public void setDriversLicenseState(String DriversLicenseState) {
+        this.DriversLicenseState = DriversLicenseState;
+    }
+
+    public String getSocialSecurityNumber() {
+        return SocialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String SocialSecurityNumber) {
+        this.SocialSecurityNumber = SocialSecurityNumber;
+    }
+
+    public String getAddressLine1() {
+        return AddressLine1;
+    }
+
+    public void setAddressLine1(String AddressLine1) {
+        this.AddressLine1 = AddressLine1;
+    }
+
+    public String getAddressLine2() {
+        return AddressLine2;
+    }
+
+    public void setAddressLine2(String AddressLine2) {
+        this.AddressLine2 = AddressLine2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZIP() {
+        return ZIP;
+    }
+
+    public void setZIP(String ZIP) {
+        this.ZIP = ZIP;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String Telephone) {
+        this.Telephone = Telephone;
+    }
+
+    public String getFaxNumber() {
+        return FaxNumber;
+    }
+
+    public void setFaxNumber(String FaxNumber) {
+        this.FaxNumber = FaxNumber;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getOriginCountry() {
+        return OriginCountry;
+    }
+
+    public void setOriginCountry(String OriginCountry) {
+        this.OriginCountry = OriginCountry;
+    }
+
+    public String getEthnicity() {
+        return Ethnicity;
+    }
+
+    public void setEthnicity(String Ethnicity) {
+        this.Ethnicity = Ethnicity;
+    }
+
+    public String getPassportNumber() {
+        return PassportNumber;
+    }
+
+    public void setPassportNumber(String PassportNumber) {
+        this.PassportNumber = PassportNumber;
+    }
+
+    public String getLanguage() {
+        return Language;
+    }
+
+    public void setLanguage(String Language) {
+        this.Language = Language;
+    }
+    
+}
